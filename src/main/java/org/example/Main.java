65759ab308c0e9f270ff3e7c627635a6e754e77a
student <student@itpark>
2022-06-04 19:19:23 +0300

    basic web-server with fixed response

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
-import java.awt.event.WindowAdapter;
+import com.google.common.primitives.Bytes;
+import org.example.exception.BadRequestException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -39,27 +41,23 @@ public class Main {
                 final InputStream in = socket.getInputStream();
         ) {
             System.out.println(socket.getInetAddress());
-            out.write("Enter command\n".getBytes(StandardCharsets.UTF_8));
+
 
             //внутренный цикл
             final String message = readMessage(in);
             System.out.println("message = " + message);
-            switch (message){
-                case "time" :
-                    final Instant now = Instant.now();
-                    out.write(now.toString().getBytes(StandardCharsets.UTF_8));
-                    break;
-                case "shutdown":
-                    out.write("Ok, shutdown server".getBytes(StandardCharsets.UTF_8));
-                    System.exit(0); // danger, finally не сработает
-                    break;
-                default:
-                    out.write("Unknown command\n".getBytes(StandardCharsets.UTF_8));
-            }
+
+            final String response = "HTTP/1.1 200 OK\r\n" +
+                            "Connection: close\r\n" +
+                            "Content-Length: 2\r\n" +
+                            "\r\n" +
+                            "OK";
+            out.write(response.getBytes(StandardCharsets.UTF_8));
         }
     }
 
     private static String readMessage(InputStream in) throws IOException {
+        final byte[] CRLFCRLF = {'\r', '\n', '\r', '\n'};// move to constants
         final byte[] buffer = new byte[4096];
         int offset = 0;
         int length = buffer.length;
@@ -67,11 +65,14 @@ public class Main {
             final int read = in.read(buffer, offset, length); // read - сколько байт было прочитано
             offset +=read;
             length = buffer.length - offset;
-
-            final byte lastByte = buffer[offset -1];
-
-            if (lastByte == '\n') { // 13 - \r, 10 - \n
+            final int headersEndIndex = Bytes.indexOf(buffer, CRLFCRLF);
+            if (headersEndIndex != -1){
                 break;
+
+            }
+
+            if (read == 0 || length == 0){
+                throw  new BadRequestException("CRLFCRLF not found");
             }
         }
         final String message = new String(
