224c330a1bdab2120cbbf409a230de8c0918aeee
student <student@itpark>
2022-06-05 16:19:54 +0300

    exposed server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
+        server.setPort(7777);
         server.start();
     }
 
